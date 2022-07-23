cfbbc00e72d41210cc213f97d671e7e5809e83c9
Eliel <elielrj@gmail.com>
2022-07-23 11:22:25 -0300

    Usuario_padrao_late

--- lib/model/bo/usuario/usuario_padrao.dart
+++ lib/model/bo/usuario/usuario_padrao.dart
@@ -5,8 +5,8 @@ import 'package:levv/model/bo/usuario/usuario.dart';
 import 'celular.dart';
 
 abstract class UsuarioPadrao implements Usuario {
-   Celular? celular;
-   TipoDeUsuario? tipoDeUsuario;
+   late Celular celular;
+   late TipoDeUsuario tipoDeUsuario;
 
 
 }
